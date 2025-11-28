561f554fc84e5b9770d87589541ff3a67fc5ca8e
Willis Chiu <willischiu214@gmail.com>
2025-11-28 10:41:23 +0000

    feat: add dep script

---
+++ pynq-zu/vivado/vivado_dep_2022-2.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget http://ftp.uk.debian.org/debian/pool/main/n/ncurses/libtinfo5_6.4-4_amd64.deb
+wget http://ftp.uk.debian.org/debian/pool/main/n/ncurses/libncurses5_6.4-4_amd64.deb
+sudo apt install ./libtinfo5_6.4-4_amd64.deb ./libncurses5_6.4-4_amd64.deb
